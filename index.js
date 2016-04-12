0ff6d1b74e174a938c27f7b5fbd77b7d497fc855
Mike Geldard <mike.geldard@degree53.com>
2016-04-12 15:13:47 +0100

    Updated for eslint v2.0

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
 module.exports = {
 
-    "parser": "babel-eslint",
+	"parser": "babel-eslint",
+	
+	"parserOptions": {
+		"ecmaFeatures": {
+			"globalReturn": false
+		},
+		"ecmaVersion": 6,
+		"sourceType": "module"
+	}
 
-    "ecmaFeatures": {
-        "arrowFunctions": true,
-        "blockBindings": true,
-        "classes": true,
-        "defaultParams": true,
-        "destructuring": true,
-        "modules": true,
-        "objectLiteralDuplicateProperties": false,
-        "restParams": true,
-        "spread": true,
-        "templateStrings": true,
-        "globalReturn": false
-    },
-
-    "rules": {
-        "arrow-body-style": [ 2, "as-needed" ],
-        "arrow-parens": [ 2, "as-needed" ],
-        "arrow-spacing": [ 2, {
+	"rules": {
+		"arrow-body-style": [ 2, "as-needed" ],
+		"arrow-parens": [ 2, "as-needed" ],
+		"arrow-spacing": [ 2, {
 			"after": true,
 			"before": true
 		}],
-        "constructor-super": 2,
-        "generator-star-spacing": [ 2, {
+		"constructor-super": 2,
+		"generator-star-spacing": [ 2, {
 			"after": true,
 			"before": true
 		}],
-        "no-arrow-condition": 2,
-        "no-class-assign": 2,
-        "no-const-assign": 2,
-        "no-dupe-class-members": 2,
-        "no-this-before-super": 2,
-        "no-var": 2,
-        "object-shorthand": [ 2, "always" ],
-        "prefer-arrow-callback": 2,
-        "prefer-const": 2,
-        "prefer-reflect": 0, // Requires polyfill
-        "prefer-spread": 2,
-        "prefer-template": 2,
-        "require-yield": 2
-    }
+		"no-class-assign": 2,
+		"no-confusing-arrow": [ 2, {
+			"allowParens": false
+		}],
+		"no-const-assign": 2,
+		"no-constant-condition": 2,
+		"no-dupe-class-members": 2,
+		"no-this-before-super": 2,
+		"no-var": 2,
+		"object-shorthand": [ 2, "always" ],
+		"prefer-arrow-callback": 2,
+		"prefer-const": 2,
+		"prefer-reflect": 0, // Requires polyfill
+		"prefer-spread": 2,
+		"prefer-template": 2,
+		"require-yield": 2
+	}
 
 };
