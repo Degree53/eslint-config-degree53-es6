c37ba41b876355bcc5b9a6fdf562daa31bb0fee1
Mike Geldard <mike.geldard@degree53.com>
2017-05-11 16:39:42 +0100

    updated to eslint@3.19.0

--- index.js
+++ index.js
@@ -6,37 +6,67 @@ module.exports = {
 	},
 
 	"rules": {
-		"arrow-body-style": [ 2, "as-needed" ],
-		"arrow-parens": [ 2, "as-needed" ],
+		"arrow-body-style": [ 2, "as-needed", {
+			"requireReturnForObjectLiteral": false
+		}],
+		"arrow-parens": [ 2, "as-needed", {
+			"requireForBlockBody": false
+		}],
 		"arrow-spacing": [ 2, {
 			"after": true,
 			"before": true
 		}],
 		"constructor-super": 2,
-		"generator-star-spacing": [ 2, "both" ],
+		"generator-star-spacing": [ 2, {
+			"after": true,
+			"before": true
+		}],
 		"no-class-assign": 2,
 		"no-confusing-arrow": [ 2, {
-			"allowParens": false
+			"allowParens": true
 		}],
 		"no-const-assign": 2,
 		"no-dupe-class-members": 2,
-		"no-duplicate-imports": 2,
+		"no-duplicate-imports": [ 2, {
+			"includeExports": true
+		}],
 		"no-new-symbol": 2,
 		"no-restricted-imports": 0,
 		"no-this-before-super": 2,
+		"no-useless-computed-key": 2,
 		"no-useless-constructor": 2,
-		"no-var": 2,
-		"object-shorthand": [ 2, "always" ],
-		"prefer-arrow-callback": 2,
-		"prefer-const": [ 2, {
-			"destructuring": "any"
+		"no-useless-rename": [ 2, {
+			"ignoreDestructuring": false,
+			"ignoreImport": false,
+			"ignoreExport": false
 		}],
-		"prefer-reflect": 0, // Requires babel polyfill
+		"no-var": 2,
+		"object-shorthand": [ 2, "always", {
+			"avoidExplicitReturnArrows": false,
+			"avoidQuotes": false,
+			"ignoreConstructors": false
+		}],
+		"prefer-arrow-callback": [ 2, {
+			"allowNamedFunctions": true,
+			"allowUnboundThis": false
+		}],
+		"prefer-const": [ 2, {
+			"destructuring": "any",
+			"ignoreReadBeforeAssign": false
+		}],
+		"prefer-destructuring": 0,
+		"prefer-numeric-literals": 2,
+		"prefer-rest-params": 2,
 		"prefer-spread": 2,
 		"prefer-template": 2,
 		"require-yield": 2,
+		"rest-spread-spacing": 0,
+		"sort-imports": 0,
+		"symbol-description": 2,
 		"template-curly-spacing": [ 2, "never" ],
-		"yield-star-spacing": [ 2, "both" ]
+		"yield-star-spacing": [ 2, {
+			"after": true,
+			"before": true
+		}]
 	}
-
 };
